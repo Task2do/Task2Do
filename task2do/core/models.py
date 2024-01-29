5606f1240f59e19cabfa9f36444069179a370ae5
AlmogShor <75311629+AlmogShor@users.noreply.github.com>
2024-01-30 01:36:23 +0200

    first step on back side

--- task2do/core/models.py
+++ task2do/core/models.py
@@ -1,18 +1,37 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
 
-class User(models.Model):
+class Worker(models.Model):
     # Additional user fields if needed
     user = models.OneToOneField(AbstractUser, on_delete=models.CASCADE)
 
+class Manager(models.Model):
+    pass
 class Project(models.Model):
+    """
+       Project model representing a team project with a name, a lead, and members.
+       """
     name = models.CharField(max_length=100)
-    lead = models.ForeignKey(User, on_delete=models.CASCADE, related_name='lead_projects')
-    members = models.ManyToManyField(User, related_name='member_projects')
+    description = models.TextField()
+    is_active = models.BooleanField(default=True)
+    lead = models.ForeignKey(Manager, on_delete=models.CASCADE, related_name='lead_projects')
+    members = models.ManyToManyField(Worker, related_name='member_projects')
+
+    def __str__(self):
+        return self.name
 
 class Task(models.Model):
+    """
+       Task model representing an individual task within a project.
+       """
     title = models.CharField(max_length=100)
     description = models.TextField()
-    project = models.ForeignKey(Project, on_delete=models.CASCADE, related_name='tasks')
-    assignee = models.ForeignKey(User, on_delete=models.SET_NULL, null=True, related_name='tasks')
     due_date = models.DateField()
+    status = models.CharField(max_length=50)
+    author = models.ForeignKey(Worker, on_delete=models.CASCADE, related_name='authored_tasks')
+    assignee = models.ForeignKey(Worker, on_delete=models.SET_NULL, null=True, related_name='assigned_tasks')
+    parent_task = models.ForeignKey('self', on_delete=models.CASCADE, null=True, blank=True, related_name='sub_tasks')
+    is_active = models.BooleanField(default=True)
+
+    def __str__(self):
+        return self.title
--- task2do/task2do/urls.py
+++ task2do/task2do/urls.py
@@ -16,9 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from core import views #Why is this not working? The core folder is in the same directory as the urls.py folder
+from task2do.core import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('projects/', views.project_list, name='project-list'),
+
 ]
