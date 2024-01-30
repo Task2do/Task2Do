95df6308d6e2f75e1aa6a24e3784ac0abd15778f
shira-chesler <98814442+shira-chesler@users.noreply.github.com>
2024-01-30 06:30:14 -0500

    updated to existing screens

--- task2do/core/views.py
+++ task2do/core/views.py
@@ -1,17 +1,25 @@
 from django.http import JsonResponse, HttpResponse
 from django.shortcuts import render
 from django.http import JsonResponse
-from .models import Worker, Manager, Project, Task
+from task2do.core.models import Worker, Manager, Project, Task
 from django.core import serializers
 from django.template import loader
+from django.shortcuts import render
+from datetime import datetime
+from django.contrib.auth.models import User
+from django.shortcuts import redirect
+from django.contrib import messages
+
 
 def index(request):
-    projects_list ={"project1":{"task1":150,"task2":124},"project2":{"task1":130,"task2":134},"project3":{"task1":5,"task2":15}}
+    projects_list = {"project1": {"task1": 150, "task2": 124}, "project2": {"task1": 130, "task2": 134},
+                     "project3": {"task1": 5, "task2": 15}}
     template = loader.get_template("core/templates/core/user.html")
     context = {
         "projects": projects_list,
     }
     return HttpResponse(template.render(context, request))
+
 
 # Add more views for different functionalities like creating users, tasks, etc.
 
@@ -61,6 +69,7 @@ def projects_list(request):
 
     return JsonResponse({'projects': projects_data})
 
+
 def users_list(request):
     """
     View to list all users with their related projects.
@@ -95,3 +104,35 @@ def users_list(request):
 
     return JsonResponse({'users': users_data})
 
+
+def open_screen(request):
+    context = {
+        'today_date': datetime.now().date()
+    }
+    return render(request, 'core/open_screen.html', context)
+
+
+def signup_success(request):
+    return render(request, 'core/signup_success.html')
+
+
+
+
+
+def signup_view(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        first_name = request.POST['first_name']
+        last_name = request.POST['last_name']
+        email = request.POST['email']
+
+        if User.objects.filter(username=username).exists():
+            messages.error(request, 'Username already exists. Please choose another one.')
+            return render(request, 'core/sign_up.html')
+        else:
+            User.objects.create_user(username=username, password=password, first_name=first_name, last_name=last_name,
+                                     email=email)
+            return redirect('signup_success')
+
+    return render(request, 'core/sign_up.html')
