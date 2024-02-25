a3ddb3112082c7da8d5945762127bbb026e2df13
Lior Shiboli <lior12sh@gmail.com>
2024-02-25 21:43:11 +0200

    added logo strip

--- task2do/task2do/settings.py
+++ task2do/task2do/settings.py
@@ -129,11 +129,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
-STATIC_ROOT = './task2do/static'
-STATICFILES_DIRS = [
-    # Put your static files directories here
-    'static'
-]
+
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
